fa1c91e22deb9881950b6de2238d381f9bb018af
mrT23 <tal.r@codium.ai>
2024-07-15 09:00:41 +0300

    t1

--- tests/e2e_tests/test_github_app.py
+++ tests/e2e_tests/test_github_app.py
@@ -6,37 +6,12 @@ from datetime import datetime
 from pr_agent.config_loader import get_settings
 from pr_agent.git_providers import get_git_provider
 from pr_agent.log import setup_logger, get_logger
+from tests.e2e_tests.e2e_utils import NEW_FILE_CONTENT, FILE_PATH, PR_HEADER_START_WITH, REVIEW_START_WITH, \
+    IMPROVE_START_WITH_REGEX_PATTERN, NUM_MINUTES
 
 log_level = os.environ.get("LOG_LEVEL", "INFO")
 setup_logger(log_level)
 logger = get_logger()
-
-new_content = """\
-from pr_agent import cli
-from pr_agent.config_loader import get_settings
-
-
-def main():
-    # Fill in the following values
-    provider = "github"  # GitHub provider
-    user_token = "..."  # GitHub user token
-    openai_key = "ghs_afsdfasdfsdf"  # OpenAI key
-    pr_url = "..."  # PR URL, for example 'https://github.com/Codium-ai/pr-agent/pull/809'
-    command = "/improve"  # Command to run (e.g. '/review', '/describe', 'improve', '/ask="What is the purpose of this PR?"')
-
-    # Setting the configurations
-    get_settings().set("CONFIG.git_provider", provider)
-    get_settings().set("openai.key", openai_key)
-    get_settings().set("github.user_token", user_token)
-
-    # Run the command. Feedback will appear in GitHub PR comments
-    output = cli.run_command(pr_url, command)
-    
-    print(output)
-
-if __name__ == '__main__':
-    main()
-"""
 
 
 def test_e2e_run_github_app():
@@ -61,17 +36,16 @@ def test_e2e_run_github_app():
         repo.create_git_ref(ref=f"refs/heads/{new_branch}", sha=source.commit.sha)
 
         # Get the file you want to edit
-        file_path = "pr_agent/cli_pip.py"
-        file = repo.get_contents(file_path, ref=base_branch)
+        file = repo.get_contents(FILE_PATH, ref=base_branch)
         # content = file.decoded_content.decode()
 
         # Update the file content
-        logger.info(f"Updating the file {file_path}")
+        logger.info(f"Updating the file {FILE_PATH}")
         commit_message = "update cli_pip.py"
         repo.update_file(
             file.path,
             commit_message,
-            new_content,
+            NEW_FILE_CONTENT,
             file.sha,
             branch=new_branch
         )
@@ -85,16 +59,7 @@ def test_e2e_run_github_app():
             base=base_branch
         )
 
-        # # delete the branch
-        # logger.info(f"Deleting the branch {new_branch}")
-        # repo.get_git_ref(f"heads/{new_branch}").delete()
-
-        # check the PR every minute, up to a limit of 5 minutes, to see if we got all the tool results
-        PR_HEADER_START_WITH = '### **User description**\nupdate cli_pip.py\n\n\n___\n\n### **PR Type**'
-        REVIEW_START_WITH = '## PR Reviewer Guide 🔍\n\n<table>\n<tr><td>⏱️&nbsp;<strong>Estimated effort to review</strong>:'
-        IMPROVE_START_WITH_REGEX_PATTERN = r'^## PR Code Suggestions ✨\n\n<!-- [a-z0-9]+ -->\n\n<table><thead><tr><td>Category</td>'
-
-        NUM_MINUTES = 5
+        # check every 1 minute, for 5, minutes if the PR has all the tool results
         for i in range(NUM_MINUTES):
             logger.info(f"Waiting for the PR to get all the tool results...")
             time.sleep(60)
@@ -113,9 +78,11 @@ def test_e2e_run_github_app():
         else:
             assert False, f"After {NUM_MINUTES} minutes, the PR did not get all the tool results"
 
-        # delete the branch
+        # cleanup - delete the branch
         logger.info(f"Deleting the branch {new_branch}")
         repo.get_git_ref(f"heads/{new_branch}").delete()
+
+        # If we reach here, the test is successful
         logger.info(f"Succeeded in running e2e test for GitHub app on the PR {pr.html_url}")
     except Exception as e:
         logger.error(f"Failed to run e2e test for GitHub app: {e}")
--- tests/e2e_tests/test_gitlab_webhook.py
+++ tests/e2e_tests/test_gitlab_webhook.py
@@ -8,38 +8,12 @@ import gitlab
 from pr_agent.config_loader import get_settings
 from pr_agent.git_providers import get_git_provider
 from pr_agent.log import setup_logger, get_logger
+from tests.e2e_tests.e2e_utils import NEW_FILE_CONTENT, FILE_PATH, PR_HEADER_START_WITH, REVIEW_START_WITH, \
+    IMPROVE_START_WITH_REGEX_PATTERN, NUM_MINUTES
 
 log_level = os.environ.get("LOG_LEVEL", "INFO")
 setup_logger(log_level)
 logger = get_logger()
-
-new_content = """\
-from pr_agent import cli
-from pr_agent.config_loader import get_settings
-
-
-def main():
-    # Fill in the following values
-    provider = "github"  # GitHub provider
-    user_token = "..."  # GitHub user token
-    openai_key = "ghs_afsdfasdfsdf"  # OpenAI key
-    pr_url = "..."  # PR URL, for example 'https://github.com/Codium-ai/pr-agent/pull/809'
-    command = "/improve"  # Command to run (e.g. '/review', '/describe', 'improve', '/ask="What is the purpose of this PR?"')
-
-    # Setting the configurations
-    get_settings().set("CONFIG.git_provider", provider)
-    get_settings().set("openai.key", openai_key)
-    get_settings().set("github.user_token", user_token)
-
-    # Run the command. Feedback will appear in GitHub PR comments
-    output = cli.run_command(pr_url, command)
-
-    print(output)
-
-if __name__ == '__main__':
-    main()
-"""
-
 
 def test_e2e_run_github_app():
     # GitLab setup
@@ -54,19 +28,17 @@ def test_e2e_run_github_app():
 
     try:
         # Create a new branch from the base branch
-        source_branch = project.branches.get(base_branch)
         logger.info(f"Creating a new branch {new_branch} from {base_branch}")
         project.branches.create({'branch': new_branch, 'ref': base_branch})
 
         # Get the file you want to edit
-        file_path = "pr_agent/cli_pip.py"
-        file = project.files.get(file_path=file_path, ref=base_branch)
+        file = project.files.get(file_path=FILE_PATH, ref=base_branch)
         # content = file.decode()
 
         # Update the file content
-        logger.info(f"Updating the file {file_path}")
+        logger.info(f"Updating the file {FILE_PATH}")
         commit_message = "update cli_pip.py"
-        file.content = new_content
+        file.content = NEW_FILE_CONTENT
         file.save(branch=new_branch, commit_message=commit_message)
 
         # Create a merge request
@@ -79,13 +51,7 @@ def test_e2e_run_github_app():
         })
         logger.info(f"Merge request created: {mr.web_url}")
 
-
-        # check the PR every minute, up to a limit of 5 minutes, to see if we got all the tool results
-        PR_HEADER_START_WITH = '### **User description**\nupdate cli_pip.py\n\n\n___\n\n### **PR Type**'
-        REVIEW_START_WITH = '## PR Reviewer Guide 🔍\n\n<table>\n<tr><td>⏱️&nbsp;<strong>Estimated effort to review</strong>:'
-        IMPROVE_START_WITH_REGEX_PATTERN = r'^## PR Code Suggestions ✨\n\n<!-- [a-z0-9]+ -->\n\n<table><thead><tr><td>Category</td>'
-
-        NUM_MINUTES = 5
+        # check every 1 minute, for 5, minutes if the PR has all the tool results
         for i in range(NUM_MINUTES):
             logger.info(f"Waiting for the MR to get all the tool results...")
             time.sleep(60)
@@ -106,9 +72,11 @@ def test_e2e_run_github_app():
         else:
             assert False, f"After {NUM_MINUTES} minutes, the MR did not get all the tool results"
 
-        # delete the branch
+        # cleanup - delete the branch
         logger.info(f"Deleting the branch {new_branch}")
         project.branches.delete(new_branch)
+
+        # If we reach here, the test is successful
         logger.info(f"Succeeded in running e2e test for GitLab app on the MR {mr.web_url}")
     except Exception as e:
         logger.error(f"Failed to run e2e test for GitHub app: {e}")
